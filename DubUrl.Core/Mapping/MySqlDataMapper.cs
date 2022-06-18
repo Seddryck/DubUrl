d6d0b32b5bdbe451176ebfa85006d9e577015fd2
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-06-18 18:18:24 +0200

    Add support through MySql.Data

---
+++ DubUrl.Core/Mapping/MySqlDataMapper.cs
@@ -0,0 +1,44 @@
+﻿using DubUrl.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Mapping
+{
+    internal class MySqlDataMapper : BaseMapper
+    {
+        public MySqlDataMapper(DbConnectionStringBuilder csb) : base(csb) { }
+
+        public override void ExecuteSpecific(UrlInfo urlInfo)
+        {
+            Specify("Server", urlInfo.Host);
+            if (urlInfo.Port>0)
+                Specify("Port", urlInfo.Port);
+            ExecuteAuthentification(urlInfo.Username, urlInfo.Password);
+            ExecuteDatabase(urlInfo.Segments);
+        }
+
+        protected internal void ExecuteAuthentification(string username, string password)
+        {
+            if (!string.IsNullOrEmpty(username))
+                Specify("User ID", username);
+            if (!string.IsNullOrEmpty(password))
+                Specify("Password", password);
+
+            if (string.IsNullOrEmpty(username) && string.IsNullOrEmpty(password))
+                Specify("Integrated Security", "sspi");
+        }
+
+        protected internal void ExecuteDatabase(string[] segments)
+        {
+            if (segments.Length == 1)
+                Specify("Database", segments.First());
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+
+    }
+}
---
+++ DubUrl.Testing/Mapping/MySqlDataMapperTest.cs
@@ -0,0 +1,92 @@
+﻿using DubUrl.Mapping;
+using DubUrl.Parsing;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DubUrl.Testing.Mapping
+{
+    public class MySqlDataMapperTest
+    {
+        private const string PROVIDER_NAME = "Mysql";
+
+        private DbConnectionStringBuilder ConnectionStringBuilder
+        {
+            get => ConnectionStringBuilderHelper.Retrieve(PROVIDER_NAME, MySqlClientFactory.Instance);
+        }
+
+        [Test]
+        [TestCase("host", "host")]
+        [TestCase("host", "host", "db", 1234)]
+        public void UrlInfo_Map_DataSource(string expected, string host = "host", string segmentsList = "db", int port = 0)
+        {
+            var urlInfo = new UrlInfo() { Host = host, Port = port, Segments = segmentsList.Split('/'), Username = "user" };
+            var mapper = new MySqlDataMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey("server"));
+            Assert.That(result["server"], Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("db")]
+        public void UrlInfo_Map_InitialCatalog(string segmentsList = "db", string expected = "db")
+        {
+            var urlInfo = new UrlInfo() { Segments = segmentsList.Split('/'), Username = "user" };
+            var mapper = new MySqlDataMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey("database"));
+            Assert.That(result["database"], Is.EqualTo(expected));
+        }
+
+
+        [Test]
+        public void UrlInfoWithUsernamePassword_Map_Authentication()
+        {
+            var urlInfo = new UrlInfo() { Username = "user", Password = "pwd", Segments = new[] { "db" } };
+            var mapper = new MySqlDataMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey("user id"));
+            Assert.That(result["user id"], Is.EqualTo("user"));
+            Assert.That(result, Does.ContainKey("password"));
+            Assert.That(result["password"], Is.EqualTo("pwd"));
+            Assert.That(result, Does.Not.ContainKey("Integrated Security"));
+        }
+
+        [Test]
+        public void UrlInfoWithoutUsernamePassword_Map_Authentication()
+        {
+            var urlInfo = new UrlInfo() { Username = "", Password = "", Segments = new[] { "db" } };
+            var mapper = new PgsqlMapper(ConnectionStringBuilder);
+            Assert.Catch<PlatformNotSupportedException>(() => mapper.Map(urlInfo));
+        }
+
+        [Test]
+        public void UrlInfo_Map_Options()
+        {
+            var urlInfo = new UrlInfo() { Username="user", Segments = new[] { "db" } };
+            urlInfo.Options.Add("SslCa", "myCert");
+            urlInfo.Options.Add("Persist Security Info", "true");
+
+            var mapper = new MySqlDataMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey("certificatefile"));
+            Assert.That(result["certificatefile"], Is.EqualTo("myCert"));
+            Assert.That(result, Does.ContainKey("persistsecurityinfo"));
+            Assert.That(result["persistsecurityinfo"], Is.True);
+        }
+    }
+}
