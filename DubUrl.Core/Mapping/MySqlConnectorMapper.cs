11ccdb20e9a1688619f1d184180ae5efe1ba57ad
Cédric L. Charlier <seddryck@gmail.com>
2022-06-18 18:53:11 +0200

    Add support to Mysql with MySqlConnector and MySql.Data (#3)
    * Improve specializability of the Specify method in a mapper to handle the different methodologies ConnectionStringBuilders are building their internal keywords list
    * Add support of MySqlconnector
    * Add support through MySql.Data

---
+++ DubUrl.Core/Mapping/MySqlConnectorMapper.cs
@@ -0,0 +1,53 @@
+﻿using DubUrl.Parsing;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Mapping
+{
+    internal class MySqlConnectorMapper : BaseMapper
+    {
+        public MySqlConnectorMapper(DbConnectionStringBuilder csb) : base(csb) { }
+
+        public override void ExecuteSpecific(UrlInfo urlInfo)
+        {
+            Specify("Server", urlInfo.Host);
+            if (urlInfo.Port>0)
+                Specify("Port", urlInfo.Port);
+            ExecuteAuthentification(urlInfo.Username, urlInfo.Password);
+            ExecuteDatabase(urlInfo.Segments);
+        }
+
+        protected internal void ExecuteAuthentification(string username, string password)
+        {
+            if (!string.IsNullOrEmpty(username))
+                Specify("User ID", username);
+            if (!string.IsNullOrEmpty(password))
+                Specify("Password", password);
+
+            if (string.IsNullOrEmpty(username))
+                throw new UsernameNotFoundException();
+        }
+
+        protected internal void ExecuteDatabase(string[] segments)
+        {
+            if (segments.Length == 1)
+                Specify("Database", segments.First());
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+
+        protected override void Specify(string keyword, object value)
+        {
+            if (ContainsKey(keyword))
+                throw new InvalidOperationException($"The keyword '{keyword}' is already specified for this connection string.");
+            if (value == null)
+                throw new ArgumentNullException(nameof(value), $"The value for the keyword '{keyword}' cannot be null.");
+
+            AddToken(keyword, value);
+        }
+    }
+}
