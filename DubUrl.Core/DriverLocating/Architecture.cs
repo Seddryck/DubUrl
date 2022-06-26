600e0e0fb20a378a26ffb87e741bbe9cbfd55e8c
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-06-26 17:05:45 +0200

    Add implementation of Postgresql driver locator

---
+++ DubUrl.Core/DriverLocating/Architecture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.DriverLocating
+{
+    public enum Architecture
+    {
+        Unspecified,
+        x86,
+        x64,
+    }
+}
--- DubUrl.Core/DriverLocating/PostgresqlDriverLocator.cs
+++ DubUrl.Core/DriverLocating/PostgresqlDriverLocator.cs
@@ -2,11 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DubUrl.DriverLocating
 {
-    internal class PostgresqlDriverLocator
+    internal class PostgresqlDriverLocator : BaseDriverLocator
     {
+        private const string REGEX_PATTERN = "^\\bPostgreSQL \\b(\\bANSI\\b|\\bUnicode\\b)\\(?(\\bx64\\b)?\\)?$";
+        private readonly List<string> Candidates = new();
+        private Encoding Encoding { get; }
+        private Architecture Architecture { get; }
+
+        public PostgresqlDriverLocator(Encoding encoding = Encoding.Unspecified, Architecture architecture = Architecture.Unspecified)
+           : base(REGEX_PATTERN) => (Encoding, Architecture) = (encoding, architecture);
+        internal PostgresqlDriverLocator(DriverLister driverLister, Encoding encoding = Encoding.Unspecified, Architecture architecture = Architecture.Unspecified)
+            : base(REGEX_PATTERN, driverLister) => (Encoding, Architecture) = (encoding, architecture);
+
+        protected override void AddCandidate(string driver, MatchCollection matches)
+        {
+            var encoding = (Encoding)Enum.Parse(typeof(Encoding), matches[0].Groups[1].Value);
+            var architecture = (Architecture)Enum.Parse(typeof(Architecture), matches[0].Groups.Count > 1 && !string.IsNullOrEmpty(matches[0].Groups[2].Value) ? matches[0].Groups[2].Value : "x86");
+
+            if (Encoding != Encoding.Unspecified && encoding != Encoding)
+                return;
+            if (Architecture != Architecture.Unspecified && architecture != Architecture)
+                return;
+
+            Candidates.Add(driver);
+        }
+
+        protected override List<string> RankCandidates()
+            => Candidates.ToList();
     }
 }
