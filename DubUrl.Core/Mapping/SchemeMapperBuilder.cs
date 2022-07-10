cf7d908c8285f263051ae01a016f193a0b1a7343
Cédric L. Charlier <seddryck@gmail.com>
2022-07-10 22:10:39 +0200

    Implement FirebirdSqlMapper for .NET provider (#26)

--- DubUrl.Core/Mapping/SchemeMapperBuilder.cs
+++ DubUrl.Core/Mapping/SchemeMapperBuilder.cs
@@ -32,6 +32,7 @@ namespace DubUrl.Mapping
             AddSchemes("IBM.Data.DB2.Core", typeof(Db2Mapper), new[] { "db2" });
             AddSchemes("Teradata.Client", typeof(TeradataMapper), new[] { "td", "tera", "teradata" });
             AddSchemes("Snowflake.Data.Client", typeof(SnowflakeMapper), new[] { "sf", "snwoflake" });
+            AddSchemes("FirebirdSql.Data.FirebirdClient", typeof(FirebirdSqlMapper), new[] { "fb", "firebird" });
             AddSchemes("System.Data.Odbc", typeof(OdbcMapper), new[] { "odbc" });
             AddSchemes("System.Data.OleDb", typeof(OleDbMapper), new[] { "oledb" });
 
