f9f12fe1301cdd36b0c50a880c01d77eeb6aaba3
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-07-11 15:45:58 +0200

    Fix some keywords for Postgresql mapper. Add tests related to Port for Postgresql

--- DubUrl.Testing/Mapping/PgsqlMapperTest.cs
+++ DubUrl.Testing/Mapping/PgsqlMapperTest.cs
@@ -24,15 +24,29 @@ namespace DubUrl.Testing.Mapping
         [Test]
         [TestCase("host", "host")]
         [TestCase("host", "host", "db", 1234)]
-        public void UrlInfo_Map_DataSource(string expected, string host = "host", string segmentsList = "db", int port = 0)
+        public void Map_UrlInfo_DataSource(string expected, string host = "host", string segmentsList = "db", int port = 0)
         {
             var urlInfo = new UrlInfo() { Host = host, Port = port, Segments = segmentsList.Split('/') };
             var mapper = new PgsqlMapper(ConnectionStringBuilder);
             var result = mapper.Map(urlInfo);
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(result, Does.ContainKey("Host"));
-            Assert.That(result["Host"], Is.EqualTo(expected));
+            Assert.That(result, Does.ContainKey(PgsqlMapper.SERVER_KEYWORD));
+            Assert.That(result[PgsqlMapper.SERVER_KEYWORD], Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1234, 1234, "host")]
+        [TestCase(4567, 4567, "host", "db")]
+        public void Map_UrlInfo_Port(int expected, int port = 0, string host = "host", string segmentsList = "db")
+        {
+            var urlInfo = new UrlInfo() { Host = host, Port = port, Segments = segmentsList.Split('/') };
+            var mapper = new PgsqlMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey(PgsqlMapper.PORT_KEYWORD));
+            Assert.That(result[PgsqlMapper.PORT_KEYWORD], Is.EqualTo(expected));
         }
 
         [Test]
@@ -44,43 +58,42 @@ namespace DubUrl.Testing.Mapping
             var result = mapper.Map(urlInfo);
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(result, Does.ContainKey("Database"));
-            Assert.That(result["Database"], Is.EqualTo(expected));
+            Assert.That(result, Does.ContainKey(PgsqlMapper.DATABASE_KEYWORD));
+            Assert.That(result[PgsqlMapper.DATABASE_KEYWORD], Is.EqualTo(expected));
         }
 
-
         [Test]
-        public void UrlInfoWithUsernamePassword_Map_Authentication()
+        public void Map_UrlInfoWithUsernamePassword_Authentication()
         {
             var urlInfo = new UrlInfo() { Username = "user", Password = "pwd", Segments = new[] { "db" } };
             var mapper = new PgsqlMapper(ConnectionStringBuilder);
             var result = mapper.Map(urlInfo);
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(result, Does.ContainKey("Username"));
-            Assert.That(result["Username"], Is.EqualTo("user"));
-            Assert.That(result, Does.ContainKey("Password"));
-            Assert.That(result["Password"], Is.EqualTo("pwd"));
-            Assert.That(result, Does.ContainKey("Integrated Security"));
-            Assert.That(result["Integrated Security"], Is.EqualTo(false));
+            Assert.That(result, Does.ContainKey(PgsqlMapper.USERNAME_KEYWORD));
+            Assert.That(result[PgsqlMapper.USERNAME_KEYWORD], Is.EqualTo("user"));
+            Assert.That(result, Does.ContainKey(PgsqlMapper.PASSWORD_KEYWORD));
+            Assert.That(result[PgsqlMapper.PASSWORD_KEYWORD], Is.EqualTo("pwd"));
+            Assert.That(result, Does.ContainKey(PgsqlMapper.SSPI_KEYWORD));
+            Assert.That(result[PgsqlMapper.SSPI_KEYWORD], Is.EqualTo(false));
         }
 
         [Test]
-        public void UrlInfoWithoutUsernamePassword_Map_Authentication()
+        public void Map_UrlInfoWithoutUsernamePassword_Authentication()
         {
             var urlInfo = new UrlInfo() { Username = "", Password = "", Segments = new[] { "db" } };
             var mapper = new PgsqlMapper(ConnectionStringBuilder);
             var result = mapper.Map(urlInfo);
 
             Assert.That(result, Is.Not.Null);
-            Assert.That(result, Does.Not.ContainKey("Username"));
-            Assert.That(result, Does.Not.ContainKey("Password"));
-            Assert.That(result, Does.ContainKey("Integrated Security"));
-            Assert.That(result["Integrated Security"], Is.EqualTo("sspi").Or.True);
+            Assert.That(result, Does.Not.ContainKey(PgsqlMapper.USERNAME_KEYWORD));
+            Assert.That(result, Does.Not.ContainKey(PgsqlMapper.PASSWORD_KEYWORD));
+            Assert.That(result, Does.ContainKey(PgsqlMapper.SSPI_KEYWORD));
+            Assert.That(result[PgsqlMapper.SSPI_KEYWORD], Is.EqualTo("sspi").Or.True);
         }
 
         [Test]
-        public void UrlInfo_Map_Options()
+        public void Map_UrlInfo_Options()
         {
             var urlInfo = new UrlInfo() { Segments = new[] { "db" } };
             urlInfo.Options.Add("Application Name", "myApp");
