80005ac3f8e4ffa189831dfac5135533966d0d31
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-07-11 15:46:07 +0200

    Implement CockRoachMapper for .NET provider

---
+++ DubUrl.Testing/Mapping/CockRoachMapperTest.cs
@@ -0,0 +1,111 @@
+﻿using DubUrl.Mapping;
+using DubUrl.Parsing;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace DubUrl.Testing.Mapping
+{
+    public class CockRoachMapperTest
+    {
+        private const string PROVIDER_NAME = "Npgsql";
+
+        private static DbConnectionStringBuilder ConnectionStringBuilder
+        {
+            get => ConnectionStringBuilderHelper.Retrieve(PROVIDER_NAME, NpgsqlFactory.Instance);
+        }
+
+        [Test]
+        [TestCase("host", "host")]
+        [TestCase("host", "host", "db", 1234)]
+        public void Map_UrlInfo_DataSource(string expected, string host = "host", string segmentsList = "db", int port = 0)
+        {
+            var urlInfo = new UrlInfo() { Host = host, Port = port, Segments = segmentsList.Split('/') };
+            var mapper = new CockRoachMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey(CockRoachMapper.SERVER_KEYWORD));
+            Assert.That(result[CockRoachMapper.SERVER_KEYWORD], Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(1234, 1234, "host")]
+        [TestCase(4567, 4567, "host", "db")]
+        public void Map_UrlInfo_Port(int expected, int port = 0, string host = "host", string segmentsList = "db")
+        {
+            var urlInfo = new UrlInfo() { Host = host, Port = port, Segments = segmentsList.Split('/') };
+            var mapper = new CockRoachMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey(CockRoachMapper.PORT_KEYWORD));
+            Assert.That(result[CockRoachMapper.PORT_KEYWORD], Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("db", "db.bank")]
+        public void Map_UrlInfo_Database(string segmentsList, string expected)
+        {
+            var urlInfo = new UrlInfo() { Segments = segmentsList.Split('/') };
+            var mapper = new CockRoachMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey(CockRoachMapper.DATABASE_KEYWORD));
+            Assert.That(result[CockRoachMapper.DATABASE_KEYWORD], Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Map_UrlInfoWithUsernamePassword_Authentication()
+        {
+            var urlInfo = new UrlInfo() { Username = "user", Password = "pwd", Segments = new[] { "db" } };
+            var mapper = new CockRoachMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey(CockRoachMapper.USERNAME_KEYWORD));
+            Assert.That(result[CockRoachMapper.USERNAME_KEYWORD], Is.EqualTo("user"));
+            Assert.That(result, Does.ContainKey(CockRoachMapper.PASSWORD_KEYWORD));
+            Assert.That(result[CockRoachMapper.PASSWORD_KEYWORD], Is.EqualTo("pwd"));
+            Assert.That(result, Does.ContainKey(CockRoachMapper.SSPI_KEYWORD));
+            Assert.That(result[CockRoachMapper.SSPI_KEYWORD], Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Map_UrlInfoWithoutUsernamePassword_Authentication()
+        {
+            var urlInfo = new UrlInfo() { Username = "", Password = "", Segments = new[] { "db" } };
+            var mapper = new CockRoachMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.Not.ContainKey(CockRoachMapper.USERNAME_KEYWORD));
+            Assert.That(result, Does.Not.ContainKey(CockRoachMapper.PASSWORD_KEYWORD));
+            Assert.That(result, Does.ContainKey(CockRoachMapper.SSPI_KEYWORD));
+            Assert.That(result[CockRoachMapper.SSPI_KEYWORD], Is.EqualTo("sspi").Or.True);
+        }
+
+        [Test]
+        public void UrlInfo_Map_Options()
+        {
+            var urlInfo = new UrlInfo() { Segments = new[] { "db" } };
+            urlInfo.Options.Add("Application Name", "myApp");
+            urlInfo.Options.Add("Persist Security Info", "true");
+
+            var mapper = new CockRoachMapper(ConnectionStringBuilder);
+            var result = mapper.Map(urlInfo);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Does.ContainKey("Application Name"));
+            Assert.That(result["Application Name"], Is.EqualTo("myApp"));
+            Assert.That(result, Does.ContainKey("Persist Security Info"));
+            Assert.That(result["Persist Security Info"], Is.True);
+        }
+    }
+}
