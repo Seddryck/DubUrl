0db933df4af6967082bccd78766472e6e4690a4f
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-08-02 13:28:33 +0200

    Create an introspector parsing the assembly to find out the different driver locators

---
+++ DubUrl.Core/Locating/OdbcDriver/DriverLocatorIntrospector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Locating.OdbcDriver
+{
+    public class DriverLocatorIntrospector
+    {
+        public record struct DriverLocatorInfo(Type DriverLocatorType, string DatabaseName, string[] Aliases, string NamePattern, string[] Options, int ListingPriority) { }
+
+        public IEnumerable<DriverLocatorInfo> Locate()
+            => typeof(DriverLocatorFactory).Assembly
+                    .GetTypes()
+                    .Where(x => x.IsClass
+                        && x.GetCustomAttributes(typeof(DriverAttribute), false).Length > 0)
+                    .Select(x => (Type: x, Attribute: x.GetCustomAttribute<DriverAttribute>() ?? throw new InvalidOperationException()))
+                    .Select(x => new DriverLocatorInfo(
+                        x.Type,
+                        x.Attribute.DatabaseName,
+                        x.Attribute.Aliases,
+                        x.Attribute.NamePattern,
+                        x.Attribute.Options.Select(t => t.Name).ToArray(),
+                        x.Attribute.ListingPriority
+                   ));
+    }
+}
