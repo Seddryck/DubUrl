07798ed7883c81acbd33a389c8eaecdf2f319f7f
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-08-21 21:54:55 +0200

    Add Connectivity information to make distinction between native and oledb/odbc

---
+++ DubUrl.Core/Mapping/Connectivity/NativeConnectivity.cs
@@ -0,0 +1,13 @@
+﻿using DubUrl.Parsing;
+using DubUrl.Querying.Dialecting;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Mapping.Connectivity
+{
+    internal class NativeConnectivity : IDirectConnectivity { }
+}
---
+++ DubUrl.Core/Mapping/Connectivity/OleDbConnectivity.cs
@@ -0,0 +1,17 @@
+﻿using DubUrl.Parsing;
+using DubUrl.Querying.Dialecting;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Mapping.Connectivity
+{
+    [GenericConnectivity(
+        "OLE DB"
+        , new[] { "oledb" }
+    )]
+    internal class OleDbConnectivity : IGenericConnectivity { }
+}
