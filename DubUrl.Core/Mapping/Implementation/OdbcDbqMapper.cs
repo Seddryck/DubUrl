e64dcd03ed4b2b8890221f0e4ee08ea2f175c820
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-08-21 22:55:16 +0200

    Change building of path for DBQ

--- DubUrl.Core/Mapping/Implementation/OdbcDbqMapper.cs
+++ DubUrl.Core/Mapping/Implementation/OdbcDbqMapper.cs
@@ -40,12 +40,30 @@ namespace DubUrl.Mapping.Implementation
         {
             internal override void Execute(UrlInfo urlInfo)
             {
-                var fullHost = new StringBuilder(urlInfo.Host);
+                var segments = new List<string>();
+                if (string.IsNullOrEmpty(urlInfo.Host) && urlInfo.Segments.Length > 1 && string.IsNullOrEmpty(urlInfo.Segments[0]))
+                    segments = urlInfo.Segments.Skip(1).ToList();
+                else
+                {
+                    if (!(StringComparer.InvariantCultureIgnoreCase.Compare(urlInfo.Host, "localhost") == 0 || StringComparer.InvariantCultureIgnoreCase.Compare(urlInfo.Host, ".") == 0))
+                        segments.Add(urlInfo.Host);
+                    segments.AddRange(urlInfo.Segments);
+                }
 
-                foreach (var segment in urlInfo.Segments)
-                    fullHost.Append('\\').Append(segment);
+                Specificator.Execute(SERVER_KEYWORD, BuildPath(segments));
+            }
 
-                Specificator.Execute(SERVER_KEYWORD, fullHost.ToString());
+            private string BuildPath(IEnumerable<string> segments)
+            {
+                if (segments == null || segments.Count() == 0)
+                    throw new ArgumentException();
+
+                var path = new StringBuilder();
+                foreach (var segment in segments)
+                    if (!string.IsNullOrEmpty(segment))
+                        path.Append(segment).Append(Path.DirectorySeparatorChar);
+                path.Remove(path.Length - 1, 1);
+                return path.ToString();
             }
         }
 
