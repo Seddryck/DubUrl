f130b8612908ce9da96a70e7c8af8dfd35e87d5a
Cédric L. Charlier <cedric.charlier@yucom.be>
2022-09-10 18:04:40 +0200

    Add simple dependecy injection helper and corresponding QA tests

---
+++ DubUrl.Core/DatabaseUrlFactory.cs
@@ -0,0 +1,29 @@
+﻿using DubUrl.Mapping;
+using DubUrl.Parsing;
+using DubUrl.Querying;
+using DubUrl.Querying.Dialecting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl
+{
+    public class DatabaseUrlFactory
+    {
+        private ConnectionUrlFactory ConnectionUrlFactory { get; }
+        protected CommandFactory CommandFactory { get; }
+
+        public DatabaseUrlFactory(ConnectionUrlFactory connectionUrlFactory, CommandFactory commandFactory)
+            => (ConnectionUrlFactory, CommandFactory) = (connectionUrlFactory, commandFactory);
+
+        public DatabaseUrl Instantiate(string url)
+        {
+            var connectionUrl = ConnectionUrlFactory.Instantiate(url);
+            return new DatabaseUrl(connectionUrl, CommandFactory);
+        }
+    }
+}
