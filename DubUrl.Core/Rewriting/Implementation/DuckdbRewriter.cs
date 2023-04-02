fa31cf44acf89129eb06f20885b5ec0d214fa40a
Cédric L. Charlier <seddryck@gmail.com>
2023-04-03 00:22:14 +0200

    Implement basic classes supporting ADO.Net provider for DuckDB

---
+++ DubUrl.Core/Rewriting/Implementation/DuckdbRewriter.cs
@@ -0,0 +1,66 @@
+﻿using DubUrl.Parsing;
+using DubUrl.Rewriting.Tokening;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Rewriting.Implementation
+{
+    internal class DuckdbRewriter : ConnectionStringRewriter
+    {
+        protected internal const string DATABASE_KEYWORD = "Data Source";
+
+        public DuckdbRewriter(DbConnectionStringBuilder csb)
+            : base(   new SpecificatorUnchecked(csb),
+                      new BaseTokenMapper[] {
+                        new DataSourceMapper(),
+                        new OptionsMapper(),
+                      }
+            )
+        { }
+
+        internal class DataSourceMapper : BaseTokenMapper
+        {
+            public override void Execute(UrlInfo urlInfo)
+            {
+                var segments = new List<string>();
+                if (StringComparer.InvariantCultureIgnoreCase.Compare(urlInfo.Host, "memory") == 0
+                        || StringComparer.InvariantCultureIgnoreCase.Compare(urlInfo.Host, ":memory:") == 0
+                       )
+                {
+                    if (!urlInfo.Segments.Any())
+                        segments.Add(":memory:");
+                    else
+                        throw new ArgumentOutOfRangeException($"Expecting no segment in the connectionUrl because the InMemory mode was activated by specifying the host '{urlInfo.Host}' but get {urlInfo.Segments.Length} segments. The list of segments was '{string.Join("', '", urlInfo.Segments.ToArray())}'");
+                }
+                else if (string.IsNullOrEmpty(urlInfo.Host) && urlInfo.Segments.Length > 1 && string.IsNullOrEmpty(urlInfo.Segments[0]))
+                    segments = urlInfo.Segments.Skip(1).ToList();
+                else
+                { 
+                    if (!(StringComparer.InvariantCultureIgnoreCase.Compare(urlInfo.Host, "localhost") == 0
+                        || StringComparer.InvariantCultureIgnoreCase.Compare(urlInfo.Host, ".") == 0))
+                        segments.Add(urlInfo.Host);
+                    segments.AddRange(urlInfo.Segments);
+                }
+
+                Specificator.Execute(DATABASE_KEYWORD, BuildPath(segments));
+            }
+
+            private string BuildPath(IEnumerable<string> segments)
+            {
+                if (segments == null || segments.Count() == 0)
+                    throw new ArgumentException();
+
+                var path = new StringBuilder();
+                foreach (var segment in segments)
+                    if (!string.IsNullOrEmpty(segment))
+                        path.Append(segment).Append(Path.DirectorySeparatorChar);
+                path.Remove(path.Length - 1, 1);
+                return path.ToString();
+            }
+        }
+    }
+}
