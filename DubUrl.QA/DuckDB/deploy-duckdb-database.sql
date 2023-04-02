e676d2f02bb0c44829cd5ceece82008b2e991d76
Cédric L. Charlier <seddryck@gmail.com>
2023-04-03 00:24:33 +0200

    Add QA tests for DuckDB

---
+++ DubUrl.QA/DuckDB/deploy-duckdb-database.sql
@@ -0,0 +1,23 @@
+.open ./DubUrl.QA/bin/Release/net6.0/Customer.duckdb
+
+CREATE SEQUENCE CustomerIdSeq 
+	INCREMENT BY 1 
+	START 1 
+	NO CYCLE
+;
+
+CREATE TABLE Customer (
+    CustomerId INT NOT NULL PRIMARY KEY,
+    FullName VARCHAR(50),
+    BirthDate DATE
+);
+
+INSERT INTO Customer
+SELECT NextVal('CustomerIdSeq') as CustomerId, 'Nikola Tesla' as FullName, '1856-07-10' as BirthDate UNION ALL
+SELECT NextVal('CustomerIdSeq'), 'Albert Einstein',    '1879-03-14' UNION ALL
+SELECT NextVal('CustomerIdSeq'), 'John von Neumann',   '1903-12-28' UNION ALL
+SELECT NextVal('CustomerIdSeq'), 'Alan Turing',        '1912-06-23' UNION ALL
+SELECT NextVal('CustomerIdSeq'), 'Linus Torvalds',     '1969-12-28'
+RETURNING *
+;
+
