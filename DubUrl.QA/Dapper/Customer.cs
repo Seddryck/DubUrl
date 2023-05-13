12652df5b0228217b121b15396a814fab8ba2d9d
Cédric L. Charlier <seddryck@gmail.com>
2023-05-13 14:30:02 +0200

    Implement tests to assert that DubUrl and Dapper can be used together

---
+++ DubUrl.QA/Dapper/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.QA.Dapper
+{
+    internal class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; } = "";
+        public DateTime BirthDate { get; set; }
+    }
+
+
+}
---
+++ DubUrl.QA/Dapper/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.QA.Dapper
+{
+    internal interface ICustomerRepository : IRepository<Customer>
+    { }
+}
