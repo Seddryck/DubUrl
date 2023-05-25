c18936e659b1b7514dcbcd9aa31f60523938adab
Cédric L. Charlier <seddryck@gmail.com>
2023-05-25 16:36:18 +0200

    Create a dedicated script to install ODBC drivers for PostgreSQL

--- DubUrl.QA/Postgresql/deploy-pgsql-test-env.ps1
+++ DubUrl.QA/Postgresql/deploy-pgsql-test-env.ps1
@@ -36,24 +36,7 @@ if ($force -or ($filesChanged -like "*pgsql*")) {
 	& psql -U "postgres" -h "localhost" -f ".\deploy-pgsql-database.sql"
 
 	# Installing ODBC driver
-	Write-host "`tDeploying PostgreSQL ODBC drivers"
-	$drivers = Get-OdbcDriver -Name "*postgres*" -Platform "64-bit"
-	If ($drivers.Length -eq 0) {
-		Write-Host "`t`tDownloading PostgreSQL ODBC driver ..."
-		Invoke-WebRequest "https://ftp.postgresql.org/pub/odbc/versions/msi/psqlodbc_13_02_0000-x64.zip" -OutFile "$env:temp\psqlodbc.zip"
-		Write-Host "`t`tExtracting from archive PostgreSQL ODBC driver ..."
-		& 7z e "$env:temp\psqlodbc.zip" -o"$env:temp" -y
-		Write-Host "`t`tInstalling PostgreSQL ODBC driver ..."
-		& msiexec /i "$env:temp\psqlodbc_x64.msi" /quiet /qn /norestart /log "$env:temp\install-pgsql.log" | Out-Host
-		#Get-Content "$env:temp\install-pgsql.log"
-		Write-Host "`t`tChecking installation ..."
-		Get-OdbcDriver -Name "*postgres*"
-		Write-Host "`tDeployment of PostgreSQL ODBC driver finalized."
-	} else {
-		Write-Host "`t`tDrivers already installed:"
-		Get-OdbcDriver -Name "*postgres*" -Platform "64-bit"
-		Write-Host "`t`tSkipping installation of new drivers"
-	}
+	. $PSScriptRoot\deploy-pgsql-odbc-driver.ps1
 
 	# Running QA tests
 	Write-Host "Running QA tests related to PostgreSQL"
