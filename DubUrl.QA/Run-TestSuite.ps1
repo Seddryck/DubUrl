0105f87106a25e48c847d7c9047d6a02e22012ca
Cédric L. Charlier <seddryck@gmail.com>
2023-05-29 18:11:09 +0200

    Returns the count of of failed tests

--- DubUrl.QA/Run-TestSuite.ps1
+++ DubUrl.QA/Run-TestSuite.ps1
@@ -22,12 +22,12 @@ Function Run-TestSuite {
 			foreach ($category in $categories) {
 				Write-Host "`tRunning test-suite for $category"
 				$args  = @("test", "..\..\DubUrl.QA")
-				$args += "--filter"
-				$args += "`"TestCategory=$($category.Split("+") -join "`"`"&`"`"TestCategory=")`""
-				$args += @("-c", $config, "--no-build", "--nologo")
+				$args += @("--filter", "`"TestCategory=$($category.Split("+") -join "`"`"&`"`"TestCategory=")`"")
+				$args += @("-c", $config)
+				$args += @("--no-build", "--nologo")
 				$args += $adapters
 				& dotnet $args | Out-Host
-				$testSuccessful += ($lastexitcode -gt 0)
+				$testSuccessful += $lastexitcode
 			}
 		}
     }
