5e33f98e4505fd8fb3691c7b4d91ccdf7151266f
Cédric L. Charlier <seddryck@gmail.com>
2023-06-29 21:05:11 +0200

    Handling of subtemplates with arguments

--- DubUrl.Testing/Querying/Templating/StringTemplateEngineTest.cs
+++ DubUrl.Testing/Querying/Templating/StringTemplateEngineTest.cs
@@ -1,4 +1,5 @@
-﻿using DubUrl.Querying.Templating;
+﻿using Amazon.Runtime.Internal.Transform;
+using DubUrl.Querying.Templating;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -62,6 +63,20 @@ namespace DubUrl.Testing.Querying.Templating
         }
 
         [Test]
+        public void Render_WithChainingSubTemplateWithVariables_Correct()
+        {
+            var engine = new StringTemplateEngine();
+            var parameters = new Dictionary<string, object?>() { { "name", "Cédric" }, { "template", "print_end" } };
+            var subTemplates = new Dictionary<string, string>()
+            {
+                { "print_name", "dear $(template)()$" },
+                { "print_end", "$name$!" }
+            };
+            var response = engine.Render("Hello $print_name()$", subTemplates, parameters, null);
+            Assert.That(response, Is.EqualTo("Hello dear Cédric!"));
+        }
+
+        [Test]
         public void Render_WithDictionary_Correct()
         {
             var engine = new StringTemplateEngine();
@@ -73,6 +88,35 @@ namespace DubUrl.Testing.Querying.Templating
             };
             var response = engine.Render("Hello $name$ $comparison.GreaterThan$$comparison.(symbol)$", new Dictionary<string, string>(), new Dictionary<string, IDictionary<string, object?>>() { { "comparison", dictionary } }, parameters, null);
             Assert.That(response, Is.EqualTo("Hello Cédric ><"));
+        }
+
+
+        [Test]
+        public void Render_WithTemplateParsable_Correct()
+        {
+            var engine = new StringTemplateEngine();
+            var templates = new Dictionary<string, string>()
+            {
+                { "bold", "bold(value)::=<b>$value$</b>" },
+                { "italic", "italic(value)::=<i>$value$</i>" }
+            };
+            var parameters = new Dictionary<string, object?>() { { "name", "Cédric" }, { "template", "italic" } };
+            var response = engine.Render("Hello $(template)(name)$", templates, new Dictionary<string, IDictionary<string, object?>>(), parameters, null);
+            Assert.That(response, Is.EqualTo("Hello <i>Cédric</i>"));
+        }
+
+        [Test]
+        public void Render_WithTemplateParsableMultiVar_Correct()
+        {
+            var engine = new StringTemplateEngine();
+            var templates = new Dictionary<string, string>()
+            {
+                { "bold", "bold(value,value2)::=<b>$value$</b>" },
+                { "italic", "italic (value, value2) ::=<i>$value$</i>$value2$" }
+            };
+            var parameters = new Dictionary<string, object?>() { { "name", "Cédric" }, { "template", "italic" }, { "punc", "!" } };
+            var response = engine.Render("Hello $(template)(name, punc)$", templates, new Dictionary<string, IDictionary<string, object?>>(), parameters, null);
+            Assert.That(response, Is.EqualTo("Hello <i>Cédric</i>!"));
         }
     }
 }
