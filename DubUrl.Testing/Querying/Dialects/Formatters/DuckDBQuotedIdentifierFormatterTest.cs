37503b34e4e2fbc507da8ed03ee0c137001fda72
Cédric L. Charlier <seddryck@gmail.com>
2023-07-31 14:07:10 +0200

    Restrict the list of identifiers to be quoted for DuckDB (#488)
    Define the list of identifier to quote for DuckDB

---
+++ DubUrl.Testing/Querying/Dialects/Formatters/DuckDBQuotedIdentifierFormatterTest.cs
@@ -0,0 +1,56 @@
+﻿using DubUrl.Querying.Dialects.Formatters;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Testing.Querying.Dialects.Formatters
+{
+    public class DuckDBQuotedIdentifierFormatterTest
+    {
+        [TestCase("ALL")] //reserved keyword
+        [TestCase("all")] //reserved keyword
+        [TestCase("ASOF")] //reserved except function name
+        [TestCase("asof")] //reserved except function name
+        [TestCase("SEMI")] //reserved except type name
+        [TestCase("semi")] //reserved except type name
+        public void Format_Keyword_Quoted(string keyword)
+            => Assert.That(new DuckDBQuotedIdentifierFormatter().Format(keyword), Does.StartWith("\"").And.EndsWith("\""));
+
+        [TestCase("BETWEEN")] //reserved except column name
+        [TestCase("ALSO")] //unreserved
+        [TestCase("ColumnName")] //any
+        public void Format_Keyword_NotQuoted(string keyword)
+            => Assert.That(new DuckDBQuotedIdentifierFormatter().Format(keyword), Does.Not.StartWith("\"").And.Not.EndsWith("\""));
+
+
+        [TestCase("Name%")] 
+        [TestCase("+Name")] 
+        [TestCase("Na-me")] 
+        [TestCase("Name()")]
+        [TestCase("Name[]")]
+        [TestCase("Name{}")]
+        [TestCase("Name/")]
+        [TestCase("Name\\")]
+        public void Format_Symbol_Quoted(string keyword)
+            => Assert.That(new DuckDBQuotedIdentifierFormatter().Format(keyword), Does.StartWith("\"").And.EndsWith("\""));
+
+        [TestCase("Name_")] 
+        [TestCase("_Name")] 
+        [TestCase("Na_me")]
+        public void Format_Underscore_NotQuoted(string keyword)
+            => Assert.That(new DuckDBQuotedIdentifierFormatter().Format(keyword), Does.Not.StartWith("\"").And.Not.EndsWith("\""));
+
+        [TestCase("123ABC")]
+        public void Format_StartWithDigit_Quoted(string keyword)
+            => Assert.That(new DuckDBQuotedIdentifierFormatter().Format(keyword), Does.StartWith("\"").And.EndsWith("\""));
+
+        [TestCase("Name123")]
+        [TestCase("Na123me")]
+        [TestCase("_123Name")]
+        public void Format_NotStartingByDigit_NotQuoted(string keyword)
+    => Assert.That(new DuckDBQuotedIdentifierFormatter().Format(keyword), Does.Not.StartWith("\"").And.Not.EndsWith("\""));
+    }
+}
