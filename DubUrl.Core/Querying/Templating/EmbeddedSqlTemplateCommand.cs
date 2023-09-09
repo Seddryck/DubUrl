a6f2e58054a230aef64a1640a7c98f117399f847
Cédric L. Charlier <seddryck@gmail.com>
2023-09-09 15:59:34 +0200

    Some cleanup in ctors

--- DubUrl.Core/Querying/Templating/EmbeddedSqlTemplateCommand.cs
+++ DubUrl.Core/Querying/Templating/EmbeddedSqlTemplateCommand.cs
@@ -26,9 +26,6 @@ namespace DubUrl.Querying.Templating
         public EmbeddedSqlTemplateCommand(string basePath, IDictionary<string, object?> parameters, IQueryLogger queryLogger)
             : this(new EmbeddedSqlTemplateResourceManager(Assembly.GetCallingAssembly()), basePath, string.Empty, string.Empty, parameters, queryLogger) { }
 
-        public EmbeddedSqlTemplateCommand(string basePath, string subTemplatesPath, string dictionariesPath, IDictionary<string, object?> parameters, IQueryLogger queryLogger)
-            : this(new EmbeddedSqlTemplateResourceManager(Assembly.GetCallingAssembly()), basePath, subTemplatesPath, dictionariesPath, parameters, queryLogger) { }
-
         internal EmbeddedSqlTemplateCommand(IResourceTemplateManager resourceManager, string basePath, string subTemplatesPath, string dictionariesPath, IDictionary<string, object?> parameters, IQueryLogger queryLogger)
             : base(resourceManager, basePath, queryLogger)
         {
