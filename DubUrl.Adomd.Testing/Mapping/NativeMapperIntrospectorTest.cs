2c4da2e782c03649b707f12339c2e015ca6a0f05
Cédric L. Charlier <seddryck@gmail.com>
2023-10-18 07:48:50 +0200

    tests for NativeMapperIntrospectorTest

---
+++ DubUrl.Adomd.Testing/Mapping/NativeMapperIntrospectorTest.cs
@@ -0,0 +1,62 @@
+﻿using DubUrl.Mapping;
+using DubUrl.Adomd.Mapping;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DubUrl.Adomd.Testing.Mapping
+{
+    public class NativeMapperIntrospectorTest
+    {
+        internal class FakeMappersIntrospector : AssemblyTypesProbe
+        {
+            private Type[] Types { get; }
+
+            public FakeMappersIntrospector(Type[] types)
+                => Types = types;
+
+            public override IEnumerable<Type> Locate()
+                => Types;
+        }
+
+        [Test]
+        public void Locate_OneMapperClass_ClassReturned()
+        {
+            var types = new FakeMappersIntrospector(new[] { typeof(PowerBiDesktopMapper), typeof(PowerBiDesktopDatabase) });
+            var introspector = new AdomdMapperIntrospector(types);
+            var result = introspector.Locate();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.ElementAt(0).MapperType, Is.EqualTo(typeof(PowerBiDesktopMapper)));
+        }
+
+        [Test]
+        public void Locate_OneMapperClassWithBrand_ClassReturned()
+        {
+            var types = new FakeMappersIntrospector(new[] { typeof(PowerBiDesktopMapper), typeof(PowerBiDesktopDatabase) });
+            var introspector = new AdomdMapperIntrospector(types);
+            var result = introspector.Locate();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(result.ElementAt(0).Slug, Is.EqualTo("powerbi"));
+            Assert.That(result.ElementAt(0).MainColor.Length, Is.EqualTo(7));
+            Assert.That(result.ElementAt(0).SecondaryColor.Length, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Locate_TwoMapperClasses_ClassesReturned()
+        {
+            var types = new FakeMappersIntrospector(new[] { typeof(PowerBiDesktopMapper), typeof(PowerBiPremiumMapper), typeof(PowerBiDesktopDatabase), typeof(PowerBiPremiumDatabase) });
+            var introspector = new AdomdMapperIntrospector(types);
+            var result = introspector.Locate();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count(), Is.EqualTo(2));
+        }
+    }
+}
