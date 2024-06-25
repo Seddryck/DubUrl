08ccba2f607dd8cb1364eccbaa915f8661fc9383
Cédric L. Charlier <seddryck@gmail.com>
2024-06-25 11:51:51 +0200

    chore: bump DuckDB from 0.10.x to 1.0.x (#1016)

--- DubUrl.QA/DuckDB/deploy-duckdb-test-env.ps1
+++ DubUrl.QA/DuckDB/deploy-duckdb-test-env.ps1
@@ -9,7 +9,7 @@ if ($force) {
 	Write-Host "Enforcing QA testing for DuckDB"
 }
 
-$rootUrl = "https://github.com/duckdb/duckdb/releases/download/v0.10.2/"
+$rootUrl = "https://github.com/duckdb/duckdb/releases/download/v1.0.0/"
 if (-not($env:PATH -like "7-zip")) {
 	$env:PATH += ";C:\Program Files\7-Zip"
 }
