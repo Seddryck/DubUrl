e42141e84b4830088e9816a587e54f8d6b15d48e
Cédric L. Charlier <seddryck@gmail.com>
2025-05-11 13:58:51 +0200

    fix: non existing namespace in using

--- DubUrl.Core/Mapping/SchemeRegistryBuilder.cs
+++ DubUrl.Core/Mapping/SchemeRegistryBuilder.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using DubUrl.Dialect;
 using DubUrl.Locating.OdbcDriver;
 using DubUrl.Querying.Dialects;
 using DubUrl.Querying.Parametrizing;
